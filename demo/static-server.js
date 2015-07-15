e763d9fa646ce37d800649b990ee03b23bb1a8ed
nelsonic <contact.nelsonic@gmail.com>
2015-07-15 11:12:09 +0100

    adds borrows server code from http://git.io/vm0Fn (our other example)

--- demo/static-server.js
+++ demo/static-server.js
@@ -1,5 +1,22 @@
-var Hapi = require('hapi'), Path = require('path'), server = new Hapi.Server(), port = process.env.PORT || 8000;
-server.connection({ port: port });
-server.route( { method: 'GET', path: '/{param*}', handler: { directory: { path: Path.normalize(__dirname + '/') } } });
-server.start(function() { console.log('Static Server on : http://127.0.0.1:' +port) });
-console.log(">>> " + Path.normalize(__dirname + '/inxed.html'));
+var Hapi = require('hapi');
+var Path = require('path');
+var server = new Hapi.Server();
+
+server.connection({
+	host: '0.0.0.0',
+	port: Number(process.env.PORT)
+});
+
+server.route([
+  // { method: 'GET', path: '/load',      handler: require('./lib/load_messages').load },
+  { method: 'GET', path: '/{param*}', handler: { directory: { path: Path.normalize(__dirname + '/') } } }
+]);
+
+server.start(function () {
+	// require('./lib/chat').init(server.listener, function(){
+	// 	console.log(process.env.REDISCLOUD_URL);
+	// 	console.log('What do you want to talk about...?', 'listening on: http://127.0.0.1:'+process.env.PORT);
+	// });
+});
+
+module.exports = server;
